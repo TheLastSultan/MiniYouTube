4541af78903d3363384288d1fd7694c51b08c010
Meezo <mozeiny@gmail.com>
2018-09-29 21:59:27 -0700

    lists made successfully

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 const API_KEY = 'AIzaSyDKmKef_v_zyH5oKhYwddDdZcpnO8ZmK1M';
 
 
@@ -10,13 +12,16 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {videos: []};
-    YTSearch({key: API_KEY, term: 'Tarintino'}, (data) => {this.setState({videos: data})});
+    YTSearch({key: API_KEY, term: 'Tarintino'}, (tarintino) => {this.setState({videos: tarintino})});
   } 
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
