d7ed3c26d3c5e3ece7357ee8c6609b3555ed2c0f
Meezo <mozeiny@gmail.com>
2018-09-29 23:03:42 -0700

    midtroubleshoot of videos not appearing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDKmKef_v_zyH5oKhYwddDdZcpnO8ZmK1M';
 
@@ -18,6 +19,7 @@ class App extends React.Component{
     return (
       <div>
         <SearchBar />
+        <videoDetail video={this.state.videos[0]} /> 
         <VideoList videos={this.state.videos} />
       </div>
     );
